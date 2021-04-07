06a1eda49da647c8898db96cffea26646e69e340
Muhammad Mamoor Khan <22554@global.ul.com>
2021-04-07 21:01:17 +0100

    Add project files.

---
+++ GaymersBot/Program.cs
@@ -0,0 +1,103 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using GaymersBot.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GaymersBot
+{
+    internal class Program
+    {
+        private static IConfiguration Configuration { get; set; }
+
+        private static CommandService _commands;
+        private static DiscordSocketClient _client;
+
+        private static async Task Main(string[] args)
+        {
+            _commands = new CommandService();
+            _client = new DiscordSocketClient();
+
+            Console.WriteLine("This is the whey!");
+
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+                b.AddTimers();
+                b.AddServiceBus(sbOptions =>
+                {
+                    sbOptions.ConnectionString = Configuration["AzureWebJobsServiceBus"];
+                });
+            });
+
+            builder.ConfigureServices(async (context, s) =>
+            {
+                ConfigureServices(s);
+
+                var token = Configuration["Token"];
+                _client.Log += LogAsync;
+                await _client.LoginAsync(TokenType.Bot, token);
+                await _client.StartAsync();
+
+                s.AddSingleton(_client)
+                .AddSingleton(_commands)
+                .AddSingleton<LoggingService>()
+                .AddSingleton<CommandHandlingService>()
+                .BuildServiceProvider();
+            });
+
+            builder.ConfigureLogging(logging =>
+            {
+                var appInsightsKey = Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                if (!string.IsNullOrEmpty(appInsightsKey))
+                {
+                    // This uses the options callback to explicitly set the instrumentation key.
+                    logging.AddApplicationInsights(appInsightsKey)
+                        .SetMinimumLevel(LogLevel.Information);
+                    logging.AddApplicationInsightsWebJobs(o => { o.InstrumentationKey = appInsightsKey; });
+                }
+            });
+
+            var tokenSource = new CancellationTokenSource();
+            var ct = tokenSource.Token;
+            var host = builder.Build();
+            using (host)
+            {
+                //await host.Services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+                await host.RunAsync(ct);
+                tokenSource.Dispose();
+            }
+        }
+
+        private static Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables() //this doesn't do anything useful notice im setting some env variables explicitly.
+                .Build();  //build it so you can use those config variables down below.
+
+            services.AddMemoryCache(); //I'm using MemCache in some other classes
+            services.AddSingleton(Configuration);
+        }
+    }
+}
---
+++ GaymersBot/Services/CommandHandlingService.cs
@@ -0,0 +1,93 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace GaymersBot.Services
+{
+    public class CommandHandlingService
+    {
+        private readonly IServiceProvider _services;
+        private readonly CommandService _commands;
+        private readonly DiscordSocketClient _client;
+
+        public CommandHandlingService(IServiceProvider services)
+        {
+            _services = services;
+            _commands = services.GetRequiredService<CommandService>();
+            _client = services.GetRequiredService<DiscordSocketClient>();
+            _commands.CommandExecuted += OnCommandExecutedAsync;
+            _client.MessageReceived += HandleCommandAsync;
+            _client.MessageUpdated += MessageUpdatedAsync;
+            _client.Ready += Ready;
+        }
+
+        public async Task InitializeAsync()
+        {
+            // Pass the service provider to the second parameter of
+            // AddModulesAsync to inject dependencies to all modules
+            // that may require them.
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        public async Task OnCommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            // command is unspecified when there was a search failure (command not found); we don't care about these errors
+            if (!command.IsSpecified)
+                return;
+
+            // the command was successful, we don't care about this result, unless we want to log that a command succeeded.
+            if (result.IsSuccess)
+                return;
+
+            // the command failed, let's notify the user that something happened.
+            await context.Channel.SendMessageAsync($"error: {result}");
+
+            //var commandName = command.IsSpecified ? command.Value.Name : "A command";
+            //await _log.LogAsync(new LogMessage(LogSeverity.Info,
+            //    "CommandExecution",
+            //    $"{commandName} was executed at {DateTime.UtcNow}."));
+        }
+
+        public async Task HandleCommandAsync(SocketMessage socketMessage)
+        {
+            // Don't process the command if it was a system message
+            if (!(socketMessage is SocketUserMessage message)) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            var argPos = 0;
+
+            // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                  message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+                return;
+
+            // Create a WebSocket-based command context based on the message
+            var context = new SocketCommandContext(_client, message);
+
+            // Pass the service provider to the ExecuteAsync method for
+            // precondition checks.
+            await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: _services);
+        }
+
+        private async Task MessageUpdatedAsync(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {
+            // If the message was not in the cache, downloading it will result in getting a copy of `after`.
+            var message = await before.GetOrDownloadAsync();
+            Console.WriteLine($"{message} -> {after}");
+        }
+
+        private static Task Ready()
+        {
+            Console.WriteLine("Bot is connected!");
+            return Task.CompletedTask;
+        }
+    }
+}
